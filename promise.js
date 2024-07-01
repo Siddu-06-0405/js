6a652342d6d9234306ce534c54349770be41b373
siddu <csiddhardha0@gmail.com>
2024-07-01 18:56:10 +0530

    yare yare

--- promise.js
+++ promise.js
@@ -1,14 +1,14 @@
-var d = new Promise(function(harsha){
-    harsha("hmm");
+var d = new Promise(function(yokoso){
+    yokoso("watashino soul society");
 });
 
-function callback(){
+function callback(d){
     console.log(d);
 }
-console.log("nagarjuna")
+
 d.then(callback)
 
-var e = new Promise(function (happy){
-    happy();
+var e = new Promise(function (Aizen_Sosuke){
+    Aizen_Sosuke("yokoso");
 });
 console.log(e)//undefined promise
---
+++ timeIsSumOfNNumbers.js
@@ -0,0 +1,5 @@
+for(let i=1;i<11;i++){
+	setTimeout(()=>{
+		console.log(i);
+	},i*(i+1)*500);
+}
